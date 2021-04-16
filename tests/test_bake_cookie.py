03da7a996297bc08dd2e5c9ae737569d6da7b34d
Shamsuddin Rehmani <rehmani@usc.edu>
2021-04-15 20:31:12 -0400

    FEAT: add content to files * populate setup.cfg with user inputted values
    * add content to template tox.ini
    
    * generate template requirements using pip-compile

--- tests/test_bake_cookie.py
+++ tests/test_bake_cookie.py
@@ -9,6 +9,8 @@ def test_bake_project(cookies):
 
     result = cookies.bake(extra_context=extra_context)
 
+    print(result.project)
+
     assert result.exit_code == 0
     assert result.exception is None
     assert result.project.basename == "test_project"
