df9c9a7bcc4414a677325ae5741fc529a30a6bf6
Shamsuddin Rehmani <rehmani@usc.edu>
2021-04-21 00:20:09 -0400

    FEAT: allow user to choses doc type

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    install_requires=[
+        "cookiecutter==1.7.2",
+    ],
+)
--- {{cookiecutter.project_slug}}/docs/sphinx/conf.py
+++ {{cookiecutter.project_slug}}/docs/sphinx/conf.py
@@ -16,12 +16,13 @@
 #
 # -- Project information -----------------------------------------------------
 
-project = "cookiecutter-python"
-copyright = "2021, Shamsuddin Rehmani"
-author = "Shamsuddin Rehmani"
+project = "{{ cookiecutter.project_name }}"
+copyright = "{% now 'local', '%Y' %}, {{ cookiecutter.full_name }}"
+author = "{{ cookiecutter.full_name }}"
 
+version = "{{ cookiecutter.version }}"
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "{{ cookiecutter.version }}"
 
 
 # -- General configuration ---------------------------------------------------
@@ -46,31 +47,34 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_material"
-html_title = "Home"
+html_title = ""
 
 # Material theme options (see theme.conf for more information)
 html_theme_options = {
     # Set the name of the project to appear in the navigation.
-    "nav_title": "Cookiecutter for Python",
+    "nav_title": "{{ cookiecutter.project_name }}",
     # Set you GA account ID to enable tracking
-    "google_analytics_account": "UA-XXXXX",
+    "google_analytics_account": "",
     # Specify a base_url used to generate sitemap.xml. If not
     # specified, then no sitemap will be built.
-    "base_url": "https://project.github.io/project",
+    "base_url": "https://project.github.io/{{ cookiecutter.project_slug }}",
     # Set the color and the accent color
-    "color_primary": "blue",
-    "color_accent": "light-blue",
+    "color_primary": "blue-grey",
+    "color_accent": " teal",
     # Set the repo location to get a badge with stats
-    "repo_url": "https://github.com/rehmanis/cookiecutter-python",
-    "repo_name": "cookiecutter-python",
-    "repo_type": "gitub",
+    "repo_url": (
+        "https://github.com/{{ cookiecutter.github_username }}/"
+        "{{ cookiecutter.project_slug }}"
+    ),
+    "repo_name": "{{ cookiecutter.project_slug }}",
+    "repo_type": "github",
     # Visible levels of the global TOC; -1 means unlimited
     "globaltoc_depth": 3,
     # If False, expand all TOC entries
     "globaltoc_collapse": False,
     # If True, show hidden TOC entries
     "globaltoc_includehidden": False,
-    "heroes": {"index": "A python project template generator"},
+    # "heroes": {"index": "A python project template generator"},
     # "nav_links": [
     #     {"href": "index", "internal": True, "title": "Home"},
     # ],
