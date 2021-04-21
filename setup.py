d8e498ba0cf9edf7163e7cb2c37bedb6c7c4ccaa
Shamsuddin Rehmani <rehmani@usc.edu>
2021-04-21 19:29:47 -0400

    FIX: add pip-compile to dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,5 @@ from setuptools import setup
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    install_requires=[
-        "cookiecutter==1.7.2",
-    ],
+    install_requires=["cookiecutter==1.7.2", "pip-compile==6.0.1"],
 )
